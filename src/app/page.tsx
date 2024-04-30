3785d87efec5c5af6bc4adecca64de88b4650ff1
Gustavo Santana <gustavossw@hotmail.com>
2024-04-30 15:40:53 -0300

    feat: add promo banner to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "@/components/category-list";
 import Header from "@/components/header";
 import Search from "@/components/search";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,6 +14,18 @@ export default function Home() {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src={"/promo-banner-01.png"}
+          alt="Até 30% de desconto em pizzas."
+          width={0}
+          height={0}
+          quality={100}
+          sizes="100vw"
+          className="h-auto w-full object-contain"
+        />
       </div>
     </>
   );
--- src/components/category-list.tsx
+++ src/components/category-list.tsx
@@ -5,7 +5,7 @@ export default async function CategoryList() {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="flex items-center gap-3 overflow-x-scroll">
+    <div className=" flex items-center gap-3 overflow-x-scroll">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
