36bf3501d076d84b87c75c362bb85ab930c1f2c3
Gustavo Santana <gustavossw@hotmail.com>
2024-05-02 18:04:34 -0300

    style: add cn utils on star badge component

--- src/components/star-badge.tsx
+++ src/components/star-badge.tsx
@@ -1,13 +1,20 @@
+import { cn } from "@/lib/utils";
 import { Restaurant } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 
 interface StarBadgeProps {
   restaurant: Pick<Restaurant, "stars">;
+  className?: string;
 }
 
-export default function StarBadge({ restaurant }: StarBadgeProps) {
+export default function StarBadge({ restaurant, className }: StarBadgeProps) {
   return (
-    <div className=" flex items-center gap-0.5 rounded-full bg-accent px-2 py-0.5 text-accent-foreground dark:border-accent">
+    <div
+      className={cn(
+        " flex items-center gap-0.5 rounded-full bg-accent px-2 py-0.5 text-accent-foreground dark:border-accent",
+        className,
+      )}
+    >
       <StarIcon size={16} className="fill-yellow-400 text-yellow-400" />
 
       <span className="text-xs font-semibold">
